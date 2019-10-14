668acf755121a419905555200e84f5a6060b9c21
Pascal Gremaud <pascal.gremaud@unifr.ch>
2019-10-14 16:10:01 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const Koa = require('koa');
+const axios = require('axios');
+const fs = require('fs');
+
+const app = new Koa();
+
+const data = fs.readFileSync('stations.json');
+const stations = JSON.parse(data);
+
+const baseUrl = 'https://api.publibike.ch/v1/public/';
+const eBikeId = 2;
+
+const getStationEBikes = async (id) => {
+    const res = await axios.get(`${baseUrl}stations/${id}`);
+    let eBikes = 0;
+    for (let i = 0; i < res.data.vehicles.length; i += 1) {
+        if (res.data.vehicles[i].type.id === eBikeId) eBikes += 1;
+    }
+    return eBikes;
+}
+
+app.use(async (ctx) => {
+    try {
+        const promises = [];
+        for (let i = 0; i < stations.length; i += 1) {
+            promises.push(getStationEBikes(stations[i].id));
+        }
+        const eBikes = await Promise.all(promises);
+        let res = '';
+        for (let i = 0; i < stations.length; i += 1) {
+            res += `${stations[i].name} : ${eBikes[i]}\n`;
+        }
+        const date = new Date();
+
+        res += `${`0${date.getHours()}`.slice(-2)}:${`0${date.getMinutes()}`.slice(-2)}:${`0${date.getSeconds()}`.slice(-2)}`
+        ctx.body = res;
+
+    } catch (err) {
+        ctx.body = err;
+    }
+});
+
+app.listen(3000);
