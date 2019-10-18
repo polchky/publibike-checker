e502036694841e1aefcd7d668abd58b3779bf796
Pascal Gremaud <pascal.gremaud@unifr.ch>
2019-10-18 10:48:38 +0200

    Change both to all

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const eBikeId = 2;
 
 const getStationBikes = async (id, type) => {
     const res = await axios.get(`${url}${id}`);
+    if (type === 'all') return res.data.vehicles.length;
     let bikes = 0;
     for (let i = 0; i < res.data.vehicles.length; i += 1) {
-        if (res.data.vehicles[i].type.name === type || type === 'both') bikes += 1;
+        if (res.data.vehicles[i].type.name === type) bikes += 1;
     }
     return bikes;
 }
@@ -19,8 +20,10 @@ app.use(async (ctx) => {
     try {
         const type = ctx.request.query.type;
         delete ctx.request.query.type;
+
         const stations = ctx.request.query;
         const names = Object.keys(stations);
+
         const promises = [];
         for (let i = 0; i < names.length; i += 1) {
             promises.push(getStationBikes(stations[names[i]], type));
